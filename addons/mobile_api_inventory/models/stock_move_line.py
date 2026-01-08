f8d6c388dc702ce560b06ff17ade215b0a9f19f3
Phinehas Adams <phinehasmadams@icloud.com>
2026-01-08 16:07:45 -0600

    Rename MobileStockMoveLine to avoid namespace collision

--- addons/mobile_api_inventory/models/stock_move_line.py
+++ addons/mobile_api_inventory/models/stock_move_line.py
@@ -1,6 +1,6 @@
 from odoo import models
 
 
-class StockMoveLine(models.Model):
+class MobileStockMoveLine(models.Model):
     _inherit = ["stock.move.line", "mobile.change.log.mixin"]
     _mobile_change_log_enabled = True
